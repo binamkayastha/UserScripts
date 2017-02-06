0dda92ba1eb2d4301c32f5efb7c687c5d8f40d2d
binamkayastha <binamkayastha@gmail.com>
2017-02-06 15:43:30 -0500

    Added more compact attachments, fixed full width issue with addition button, removed trailing whitespaces

--- Inbox/CompactInbox.js
+++ Inbox/CompactInbox.js
@@ -14,42 +14,45 @@
 
 var runAlready = false;
 
-$(document).ready(function() {  
+$(document).ready(function() {
     'use strict';
-    
+
     if(runAlready) return;
     runAlready = true;
-    
+
     console.log("hi");
-    
+
     // Increase width
-    var css = '.cM{width: 95%;} .lQ{line-height: 20px !important;}'
+    var css = '.cM{width: 90%; padding-right: 4% !important;} .lQ{line-height: 20px !important;}'
     css += '.an{padding: 0px !important; margin: auto !important;}'
     css += '.jS{margin: 0px !important; min-height: 30px !important}'
-    
+
     // Auto Align
     css += '.iG{margin: auto !important; /*img*/}'
     css += '.rv{margin: auto !important;}'
     css += '.rv{margin: auto !important;}'
     css += '.hY{margin: auto !important;}'
-    
+
     // Remove shadow, add borders
     css += '.aa::before, .an::before{box-shadow: none !important; border: 1px solid #ddd; border-left: solid #ddd}'
-    
-    
+
     // Shrink Icon Sizes
     css += '.gi, .gN{width: 20px !important; height: 20px !important;}'
-    
+
     // Change Font size
     css += '.ai-b8{font-size: 15px; font-weight: bold;}'
-    
+
     // Removing/Decreasing empty space
     css += '.ai-b8{margin: 0px !important; margin-top: 8px}'
     css += '.az{padding: 5px !important; margin-bottom: 0px !important; margin-top: 10px !important}'
     css += '.kz{margin-bottom: 0px;}'
-    
-    // Shring Image attachment previews
+    css += '.g-aq.b0{padding: 2px 24px !important;}'
+
+    // Shrinking Attachments
     css += '.cq{height: 24px !important;} .cq:hover{height: 88px !important;}'
+    css += '.mp{height: 24px !important;}'
+    css += '.ga{height: 24px !important;}'
+
     // Apply Style
     var head, style;
     head = document.getElementsByTagName('head')[0];
@@ -58,8 +61,8 @@ $(document).ready(function() {
     style.type = 'text/css';
     style.innerHTML = css;
     head.appendChild(style);
-    
-    
+
+
     // Move attachment elements
     /*var attachmentElements = document.querySelectorAll(".u2");
     alert("length" + attachmentElements.length);
